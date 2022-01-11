c55ed92f2bd016ceab7e21c4aa00602be1728d52
JayPagada <jaypagada11@gmail.com>
2022-01-11 08:13:22 -0800

    created server and log req and send response

--- nodeJs-demo/index.js
+++ nodeJs-demo/index.js
@@ -1,4 +1,26 @@
 const rect = require("./rectangle.js");
+const http = require("http");
+
+// create server
+const server = http.createServer((req, res) =>{
+// for request the server
+    // console.log("req",req);
+    console.log("req.url",req.url);
+    console.log("req.method",req.method);
+    console.log("req.headers",req.headers);
+// respose the server
+    res.setHeader('Content-Type','text/html');
+    res.write("<html>");
+    res.write("<head><title>my first page</title></head>");
+    res.write("<body><h1>Hello from node.js!</h1></body>");
+    res.write("</html>");
+// we must have to end the response
+    res.end();
+//  for exit from all the req it means that stop the project
+// process.exit();
+});
+
+server.listen(3000);
 
 const solveRect = (l,b) =>{
     console.log("solving for rect with l = " + l + "and b =" + b);
