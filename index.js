1fbd093e32fc72de21604422c4998a8b6c191bf5
JayPagada <jaypagada11@gmail.com>
2022-04-13 22:40:41 +0530

    add custom middleware and also library of middleware of morgan

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
+const logger = require("./middleware/logger");
 // route file
 
-const bootcamps = require("./routes/bootcamps")
+const bootcamps = require("./routes/bootcamps");
 
 // lode env vars
 
-dotenv.config({path: "./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-// mount routers 
-app.use( "/api/v1/bootcamps", bootcamps);
+// dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-const PORT  = process.env.PORT || 5000;
+app.use(logger);
 
-app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} Mode on port is ${PORT} ` ));
+// mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} Mode on port is ${PORT} `
+  )
+);
---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+// apply custome middleware for logger
+
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+};
+module.exports = logger;
